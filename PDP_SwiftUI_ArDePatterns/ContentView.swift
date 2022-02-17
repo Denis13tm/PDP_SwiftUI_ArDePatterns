c9fc4c999656cddd9756dda3e19554f70887bf7a
Denis <mrdunacdtm@gmail.com>
2022-02-17 20:28:55 +0900

    Initial Commit

---
+++ PDP_SwiftUI_ArDePatterns/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PDP_SwiftUI_ArDePatterns
+//
+//  Created by 13 Denis on 17/02/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PDP_SwiftUI_ArDePatterns/PDP_SwiftUI_ArDePatternsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PDP_SwiftUI_ArDePatternsApp.swift
+//  PDP_SwiftUI_ArDePatterns
+//
+//  Created by 13 Denis on 17/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct PDP_SwiftUI_ArDePatternsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
